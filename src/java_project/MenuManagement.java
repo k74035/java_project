a9008b2f837cfef5b241cd9a1621ac4845953d78
k74035 <k74035@naver.com>
2023-03-18 16:38:35 +0900

    added MenuManagement.java

---
+++ src/java_project/MenuManagement.java
@@ -0,0 +1,17 @@
+package java_project;
+
+public class MenuManagement {
+	public static void main(String[] args){
+		//println으로 인사관리 메뉴가 한 문장씩 출력되도록 한다.
+		System.out.println("Employee Management System");
+        System.out.println("1. View Employee List"); 			//인사목록
+        System.out.println("2. View Employee Details");			//상세정보
+        System.out.println("3. Manage Employee Attendance");	//출퇴근관리 
+        System.out.println("4. Manage Employee Salary");		//급여관리
+        System.out.println("5. Update Employee Information");	//갱신
+        System.out.println("6. Add New Employee");				//직원추가
+        System.out.println("7. Remove Employee");				//직원삭제
+        System.out.println("8. Exit");							//메뉴 나가기
+	}
+
+}
